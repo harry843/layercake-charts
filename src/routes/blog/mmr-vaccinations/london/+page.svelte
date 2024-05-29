2e496a6e397293eb41553feefeaf40fa217b9da7
harry843 <kelleherharry@gmail.com>
2024-05-29 23:59:59 +0100

    adjust map section height on screen size

--- src/routes/blog/mmr-vaccinations/london/+page.svelte
+++ src/routes/blog/mmr-vaccinations/london/+page.svelte
@@ -71,7 +71,7 @@
 
 </script>
 
-<section bind:clientWidth={width} class="h-[92.5%] flex flex-col pt-1.5 xs:pt-2.5 md:pt-4">
+<section bind:clientWidth={width} class="h-[91%] xs:h-[92.5%] flex flex-col pt-0.5 xxs:pt-1 xs:pt-1.5 sm:pt-2.5 md:pt-4">
 	{#if width > 0}
 		<div>
 			<h1 class="text-base md:text-lg lg:text-xl xl:text-2xl font-medium text-center pt-4 mx-[9%] md:mx-[15%]">
